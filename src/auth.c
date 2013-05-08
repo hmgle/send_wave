2878059af3f0650a02c3c59942a40e5c9601126b
mingang.he <dustgle@gmail.com>
2013-05-08 23:18:51 +0800

    add auth.c auth.h

---
+++ src/auth.c
@@ -0,0 +1,11 @@
+#include "auth.h"
+
+char *urlencode(const char *src, int len, char *dst)
+{
+	return dst;
+}
+
+char *urldecode(const char *src, int len, char *dst)
+{
+	return dst;
+}
---
+++ src/auth.h
@@ -0,0 +1,7 @@
+#ifndef _AUTH_H
+#define _AUTH_H
+
+char *urlencode(const char *src, int len, char *dst);
+char *urldecode(const char *src, int len, char *dst);
+
+#endif
