173ca4b2bbe21a3dbda0cd35ba9e93f951b801d3
mingang.he <dustgle@gmail.com>
2014-03-30 11:28:46 +0800

    add photo desc

--- fanfouphoto.py
+++ fanfouphoto.py
@@ -5,12 +5,33 @@ from fanfou import Fanfou
 from config import consumer_key, client_secret
 from config import resource_owner_key, resource_owner_secret
 import sys
+import getopt
 
-if __name__ == "__main__":
+def main():
+    short_opts = 's:'
+    opts = ['desc=']
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], short_opts, opts)
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(1)
+
+    desc = None
+    for opt, arg in opts:
+        if opt in ('-s', '--desc'):
+            desc = arg
+        else:
+            print(help)
+            sys.exit(1)
+
     photo_data = sys.stdin
 
     fanfou = Fanfou(api_key=consumer_key,
                api_secret=client_secret,
                oauth_token=resource_owner_key,
                oauth_token_secret=resource_owner_secret)
-    fanfou.post_photo(photo_data)
+    fanfou.post_photo(photo_data, desc=desc)
+
+
+if __name__ == "__main__":
+    main()
